2488ffdae2fa7794cd7cc3906a78a1025e41ebb1
Moustapha <88747372+csof3cen@users.noreply.github.com>
2022-12-19 23:24:53 +0000

    feat: add products migration

---
+++ database/migrations/1671492055233_products.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'products'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('stock')
+      table.boolean('free_deliver')
+      table.double('price').notNullable()
+      table.string('shop_name').nullable()
+      table.string('title', 255).notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
